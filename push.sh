9ee349ee1779e25dbc950cf3225ba0b46ec08530
Hugh Wang <18501667737@163.com>
2020-04-21 21:18:20 +0800

    by Hugh

---
+++ push.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+git add .
+git commit -a -m 'by Hugh'
+git push origin master
---
+++ src/main/java/com/uwjx/regex/RegexDemoApplication.java
@@ -0,0 +1,13 @@
+package com.uwjx.regex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegexDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegexDemoApplication.class, args);
+    }
+
+}
